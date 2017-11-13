244c864be9f0871ac082d3df851d0a6ed6b01738
mouloudilamine <32839976+mouloudilamine@users.noreply.github.com>
2017-11-13 18:20:55 +0100

    Add files via upload

---
+++ tp3/form-jquery-validation.js
@@ -0,0 +1,21 @@
+﻿
+$(document).ready(function () { //pour chargement de la page
+   
+       $('#button').on("click", function (event) { //ajouter un "listener" à un élément
+           event.preventDefault();
+           console.log( "click" );
+   
+           if ($('#Nom').val() == "" || $('#Prenom').val() == "" || $('#Datedenaissance').val() == "" || $('#Adresse').val() == "" || $('#Mail').val() == "") {
+   
+               $('#myModal').modal("show");
+           }
+           else {
+               $(".modal-body").html('<p>"Vous etes nés le ' +$('#Datedenaissance').val()+'</p> <img style="max-width:100%" src="https://maps.googleapis.com/maps/api/staticmap?center='+$('#Adresse').val()+'&markers='+$('#Adresse').val()+'&size=800x400&zoom=13"/>');
+               $(".modal-title").text("Bienvenue "+$("#Nom").val());
+               $('#myModal').modal("show");
+   
+           }
+   
+       });
+})
+   
