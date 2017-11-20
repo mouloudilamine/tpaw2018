89dbcd4c7291425acf97984bd5cf1930e33f0e29
mouloudilamine <32839976+mouloudilamine@users.noreply.github.com>
2017-11-20 14:32:16 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -10,7 +10,7 @@ $(document).ready(function () { //pour chargement de la page
                $('#myModal').modal("show");
            }
            else {
-               $(".modal-body").html('<p>"Vous etes nés le ' +$('#Datedenaissance').val()+'</p> <img style="max-width:100%" src="https://maps.googleapis.com/maps/api/staticmap?center='+$('#Adresse').val()+'&markers='+$('#Adresse').val()+'&size=800x400&zoom=13&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/>');
+               $(".modal-body").html('<p>"Vous etes nés le ' +$('#Datedenaissance').val()+'</p> <img style="max-width:100%" src="https://maps.googleapis.com/maps/api/staticmap?center='+$('#Adresse').val()+'&markers='+$('#Adresse').val()+'&size=800x400&zoom=132&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/>');
                $(".modal-title").text("Bienvenue "+$("#Nom").val());
                $('#myModal').modal("show");
    
